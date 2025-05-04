2c0c05a95aecbdf717bd1a664819a0d5a16b1165
saunaman37 <ptiger1015@gmail.com>
2025-05-04 11:28:44 +0900

    タスクの追加機能（ボタン）

--- src/main.js
+++ src/main.js
@@ -15,8 +15,25 @@ const onClickAdd = () =>{
     p.className = "tode-item";
     p.innerText = inputText;
 
+    // button（完了）タグの生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    
+    completeButton.addEventListener("click",() => {
+        alert("完了");
+    });
+
+    // button（削除）タグの生成
+    const completDeleteButton = document.createElement("button");
+    completDeleteButton.innerText = "削除";
+    completDeleteButton.addEventListener("click",() => {
+        alert("削除");
+    });
+
     // liタグの子要素に各要素を設定
     div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(completDeleteButton);
     li.appendChild(div);
 
     // 未完了リストに追加
